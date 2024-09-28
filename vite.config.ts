ee3a67fb72ab288a798ffa61732f7f8606f13520
KudouSterain <40029631+hotrungnhan@users.noreply.github.com>
2024-09-28 08:21:14 +0000

    u

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/trading",
   css: {
     postcss: {
       plugins: [tailwindcss()],
